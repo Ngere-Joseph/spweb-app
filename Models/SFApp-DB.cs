7d9fec2308e6e7b4c31d266f772639199f2c35b9
joseph <joseph_ngere@protonmail.com>
2021-07-08 23:15:09 +0100

    initial commit

---
+++ Models/SFApp-DB.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SPWebApplication.Models
+{
+    public class SFApp_DB
+    {
+        public int ID { get; set; }
+
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public string Designation { get; set; }
+
+        public DateTime DOfEmployment { get; set; }
+
+
+    }
+
+
+}
